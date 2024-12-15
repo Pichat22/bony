73efc0849a67a5dd4d3c2e950d785134ae5b3869
pichat22 <mayoumareth@gmail.com>
2024-12-15 21:41:31 +0100

    reservation controller

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -15,11 +15,11 @@ class ReservationController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        $reservations= Reservation::all();
-        return view('reservations.index',compact('reservations'));
-    }
+    // public function index()
+    
+        // $reservations= Reservation::all();
+        // return view('reservations.index');
+    
 
     /**
      * Show the form for creating a new resource.
